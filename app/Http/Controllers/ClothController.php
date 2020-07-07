ef60cff14ad34caf391f2566c135eead67b9d969
George <quarelli.george@gmail.com>
2020-07-07 17:53:28 +0200

    controller

--- app/Http/Controllers/ClothController.php
+++ app/Http/Controllers/ClothController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cloth;
 
 class ClothController extends Controller
 {
-    //
+    public function index(){
+        $all_clothes = Cloth::all();
+        dd($all_clothes);
+    }
 }
