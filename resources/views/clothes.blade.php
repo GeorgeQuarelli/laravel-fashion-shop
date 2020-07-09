061efdb8e134dd72ada25c54960a89992c1b3ce3
George <quarelli.george@gmail.com>
2020-07-09 18:27:49 +0200

    + tabella

--- resources/views/clothes.blade.php
+++ resources/views/clothes.blade.php
@@ -3,11 +3,24 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <ul>
-                    @foreach ($clothes as $cloth)
-                        <li> {{ $cloth->brend }} </li>
-                    @endforeach
-                </ul>
+                <table class="table">
+                    <thead>
+                        <th scope="col">ID</th>
+                        <th scope="col">MARCA</th>
+                        <th scope="col">TIPO</th>
+                        <th scope="col">PREZZO</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($clothes as $cloth)
+                            <tr>
+                                <td> {{ $cloth->id }} </td>
+                                <td> {{ $cloth->brend }} </td>
+                                <td> {{ $cloth->type }} </td>
+                                <td> {{ $cloth->price }} </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
